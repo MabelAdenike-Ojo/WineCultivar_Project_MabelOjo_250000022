72e6758202ac11f75dfb1ac48736625260df7f9c
MabelAdenike-Ojo <cuss.ojomabel@gmail.com>
2026-01-21 23:13:26 +0100

    Final cleanup of project structure

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -34,4 +35,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
